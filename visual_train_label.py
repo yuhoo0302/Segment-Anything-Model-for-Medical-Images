a3b23b30f1a363a69206b5abba90896cc39d8ea6
Yuhao Huang <86473945+yuhoo0302@users.noreply.github.com>
2023-09-27 16:52:07 +0800

    Update visual_train_label.py

--- visual_train_label.py
+++ visual_train_label.py
@@ -21,7 +21,6 @@ def show_points(image, coords, labels):
         cv2.circle(ret_img, (coords[0][0],coords[0][1]), radius=5, color=(0,0,255),  thickness=-1)
     return ret_img
 
-    # 计算dice
 def dice_coefficient(y_true, y_pred):
     smooth = 1
     y_true_f = y_true.flatten()
@@ -30,9 +29,7 @@ def dice_coefficient(y_true, y_pred):
     return (2. * intersection + smooth) / (np.sum(y_true_f) + np.sum(y_pred_f) + smooth)
 
 def limit_rect(mask, box_ratio):
-    """ 判断扩大之后的目标框有没有超出图像 """
     height, width = mask.shape[0], mask.shape[1]
-    # 最大外接框
     box = find_box_from_mask(mask)
     w, h = box[2] - box[0], box[3] - box[1]
     w_ratio = w * box_ratio
@@ -51,7 +48,6 @@ def limit_rect(mask, box_ratio):
         y2 = height
     return x1, y1, x2-x1, y2-y1
 
-    # 找质心
 def find_center_from_mask(mask):
     # calculate moments of binary image
     M = cv2.moments(mask)
@@ -64,53 +60,53 @@ def find_center_from_mask_new(mask, box_ratio=2, n_fg=5, n_bg=5):
 # def get_all_point_info(mask, box_ratio, n_fg, n_bg):
     """
     input:
-        mask:     单个目标的mask
-        bg_ratio: 基于最大外接框的基础上扩张bg_ratio倍
-        n_fg:     前景点个数
-        n_bg:     背景点个数
+        mask:     single mask
+        bg_ratio: Expanded bg_ratio times based on the largest external frame
+        n_fg:     Number of foreground points
+        n_bg:     Number of background points
     Return:
-        point_coords(ndarry): size=M*2, 选取M个点(前景或背景)
-        point_labels(ndarry): size=M, M个点的Label, 1为前景, 0为背景
+        point_coords(ndarry): size=M*2, select M point (foreground or background)
+        point_labels(ndarry): size=M 
     """
-    # 找质心
+    # find center of mass
     M = cv2.moments(mask)
     cX = int(M["m10"] / M["m00"])
     cY = int(M["m01"] / M["m00"])
     center_point = np.array([cX, cY]).reshape(1, 2)
 
-    # 获得前景点
+    # acquire foreground points
     indices_fg = np.where(mask == 1)
     points_fg = np.column_stack((indices_fg[1], indices_fg[0]))
 
-    # 均匀采样n个点
+    # uniformly sample n points
     step_fg = int(len(points_fg) / n_fg)
     # print(len(points_fg))
     points_fg = points_fg[::step_fg, :]
     
 
-    # 找最大外接框
+    # find the maximum bounding box
     x, y, w, h = limit_rect(mask, box_ratio)
     box1 = (x, y, x+w, y+h)
     x, y, w, h = int(x), int(y), int(w), int(h)
 
-    # 获得背景点
+    # acquire background points
     yy, xx = np.meshgrid(np.arange(x, x+w), np.arange(y, y+h))
     roi = mask[y:y+h, x:x+w]
     bool_mask = roi == 0
     points_bg = np.column_stack((yy[bool_mask], xx[bool_mask]))
 
-    # 均匀采样n个点
+    # uniformly sample n points
     step_bg = int(len(points_bg) / n_bg)
     points_bg = points_bg[::step_bg, :]
 
-    # 获取point_coords
+    # get point_coords
     points_fg = np.concatenate((center_point, points_fg[1:]), axis=0)
     point_coords = np.concatenate((points_fg, points_bg), axis=0)
     point_labels = np.concatenate((np.ones(n_fg), np.zeros(n_bg)), axis=0)
 
     return point_coords, point_labels, points_fg, points_bg, box1, (cX, cY) 
 
-    # 找质心
+   # find center of mass
 def find_box_from_mask(mask):
     y, x = np.where(mask == 1)
     x0 = x.min()
@@ -119,7 +115,7 @@ def find_box_from_mask(mask):
     y1 = y.max()
     return [x0, y0, x1, y1]
 
-    # 获取点和box的信息
+    # acquire information about points and box
 def find_all_info(mask, label_list):
     point_list = []
     point_label_list = []
@@ -175,8 +171,7 @@ def dice_coefficient(y_true, y_pred):
 def test_single_dataset(task, dataset_list, size):
     print("Current processing task: ", task)
     
-    # 获取标签映射信息
-    info = json.load(open(f"/mnt/data1/liulian/SAM_liulian/fine_tune/data_all/{task}/{task.split('_')[1]}_2d/{task}.json"))
+    info = json.load(open(f"data_all/{task}/{task.split('_')[1]}_2d/{task}.json"))
     label_info = info["info"]
     color_info = info["color"]
     labels = []
